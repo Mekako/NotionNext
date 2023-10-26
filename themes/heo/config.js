3e4dbb0967cbd6940b204483a12938edece0445c
Meka <113497377+Mekako@users.noreply.github.com>
2023-10-26 10:23:32 +0800

    更改主题关键字

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -19,7 +19,7 @@ const CONFIG = {
 
   // 英雄区显示三个置顶分类
   HERO_CATEGORY_1: { title: '必看精选', url: '/tag/必看精选' },
-  HERO_CATEGORY_2: { title: '热门文章', url: '/tag/热门文章' },
+  HERO_CATEGORY_2: { title: '软件分享', url: '/tag/软件分享' },
   HERO_CATEGORY_3: { title: '实用教程', url: '/tag/实用教程' },
 
   // 英雄区右侧推荐文章标签, 例如 [推荐] , 最多六篇文章; 若留空白''，则推荐最近更新文章
